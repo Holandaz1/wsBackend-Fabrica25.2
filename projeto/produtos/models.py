820377f14d9c7f679037442b65bca0a7e595b703
Lucas <lucas2007zzt@gmail.com>
2025-09-13 14:38:52 -0300

    :sparkles: Criação dos models e estrutura inicial do banco de dados

--- projeto/produtos/models.py
+++ projeto/produtos/models.py
@@ -1,2 +1,12 @@
 from django.db import models
  
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+
+class Produtos(models.Model):
+    nome = models.CharField(max_length=100)
+    preço = models.DecimalField(max_digits=10, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+def __str__(self):
+    return self.nome
